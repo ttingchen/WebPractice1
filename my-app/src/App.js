2c930f71cf4c7c3bad664b294761c9ceb02a8f05
fionach <fionach38@gmail.com>
2021-01-14 16:26:06 +0800

    edit nav style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,12 +5,15 @@ function App() {
   return (
     <div class='App'>
       <div class='App-header'>
-        <div>logo</div>
+        <div>LOGO</div>
+
         <div>
           <nav>
-          <a href="/projects/">projects</a>
+          <a href="/projects/" class="nav-item">PROJECTS</a>
+          <a href="/studio/" class="nav-item">STUDIO</a>
           </nav>
         </div>
+
       </div>
         <h1> hello, {formatName(user)} !</h1>
         <h2> It is { new Date().toLocaleTimeString()}.</h2>
