493281b1efcd795ed417e7f84efe63dea821a803
ttingchen <fionach38@gmail.com>
2023-10-24 14:20:43 -0500

    enable contact fild to send email through emailJS

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // importing Link from react-router-dom to navigate to 
 // different end points.
 // import { Link } from "react-router-dom";
@@ -9,7 +9,51 @@ import pic3 from '../assets/mountain.jpg';
 import p1 from '../assets/biped.jpg';
 import defa from '../assets/p1.jpg';
 import mirror from '../assets/mirror.png';
-const Home = () => {
+import { send } from 'emailjs-com';
+
+function Home() {
+    const [toSend, setToSend] = useState({
+        from_name_first: '',
+        from_name_last: '',
+        from_email: '',
+        message: '',
+    });
+
+    const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+    };
+
+    function handleSubmit(e) {
+        // Prevent the browser from reloading the page
+        e.preventDefault();
+
+        // Read the form data
+        const form = e.target;
+        const formData = new FormData(form);
+
+        send(
+            'service_zg7xfi9',
+            'template_gi8k24h',
+            toSend,
+            'k30PFcyjgb34ExFAU'
+        )
+            .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+                handleReset();
+
+                setToSend({ messageSent: "Message Sent!" });
+            })
+            .catch((err) => {
+                console.log('FAILED...', err);
+            });
+
+        // Or you can work with it as a plain object:
+        // const formJson = Object.fromEntries(formData.entries());
+        // console.log(formJson);
+    }
+    function handleReset() {
+        setToSend({ from_email: "", from_name_first: "", from_name_last: "", message: "", messageSent: "" });
+    }
     return (
         <div>
             <div className="slideshow-container">
@@ -58,19 +102,19 @@ const Home = () => {
                 <div className="projects-text" id="contact">
                     CONTACT
                 </div>
-                <div className="form">
+                <form id="form" className="form" method="post" onSubmit={handleSubmit} onReset={handleReset}>
                     <div className="form-grid">
                         <div className="input-field">
-                            <label htmlFor="fname">FIRST NAME</label>
-                            <input className="input" type="text" id="fname" name="fname"></input>
+                            <label htmlFor="fname">FIRST NAME *</label>
+                            <input className="input" type="text" id="fname" name="from_name_first" value={toSend.from_name_first} onChange={handleChange} required ></input>
                         </div>
                         <div className="input-field">
                             <label htmlFor="lname">LAST NAME</label>
-                            <input className="input" type="text" id="lname" name="lname"></input>
+                            <input className="input" type="text" id="lname" name="from_name_last" value={toSend.from_name_last} onChange={handleChange}></input>
                         </div>
                         <div className="input-field">
-                            <label htmlFor="email">EMAIL ADDRESS</label>
-                            <input className="input" type="text" id="email" name="email"></input>
+                            <label htmlFor="email">EMAIL ADDRESS *</label>
+                            <input className="input" type="email" id="email" name="from_email" value={toSend.from_email} onChange={handleChange} required ></input>
                         </div>
                         <div className="input-field">
                             <label htmlFor="country">COUNTRY</label>
@@ -79,32 +123,28 @@ const Home = () => {
 
                     </div>
                     <div className="message-field">
-                        <label htmlFor="message">MESSAGE</label>
-                        <textarea className="message" name="message" placeholder="The cat was playing in the garden.">
+                        <label htmlFor="message">MESSAGE *</label>
+                        <textarea className="message" name="message" placeholder="Hello! I like your website." value={toSend.message} onChange={handleChange} required >
                         </textarea>
                     </div>
                     <div className="submit-container">
+                        <input className="submit" type="reset" value="Reset"></input>
                         <input className="submit" type="submit" value="Submit"></input>
                     </div>
-                </div>
+                    <div className="popup-field">
+                        <label htmlFor="message">{toSend.messageSent}</label>
+                    </div>
+                </form>
             </div>
             <div className="footer">
-                <h1> hello, {formatName(user1)} !</h1>
+                {/* <h1> hello, {formatName(user1)} !</h1> */}
+                <h1> Hello {toSend.from_name_first}!</h1>
                 <h4> {new Date().toLocaleTimeString()}.</h4>
             </div>
         </div>
     );
 };
 
-function formatName(user) {
-    return user.firstName + user.lastName;
-    //`${user.firstName} ${user.lastName}`
-}
-
-const user1 = {
-    firstName: 'ttt',
-    lastName: 'ing'
-};
 
 var slideIndex = 1;
 const picNum = 3;
